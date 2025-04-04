e72d5f27ed1deb3f920ec8738e07d0d1b506d6e8
muchamadfaiz <muchamadfaiz@gmail.com>
2025-04-04 19:58:32 +0700

    feat(auth): add password strength validation (min 8 chars, number, symbol)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,7 +22,19 @@ const registerValidateSchema = Yup.object({
   fullName: Yup.string().required(),
   username: Yup.string().required(),
   email: Yup.string().required(),
-  password: Yup.string().required(),
+  password: Yup.string()
+    .required()
+    .min(6, "Password must be at least 6 characters")
+    .test("at-least-one-uppercase-letter", "Contain at least one uppercase letter", (value) => {
+      if (!value) return false;
+      const regex = /^(?=.*[A-Z])/;
+      return regex.test(value);
+    })
+    .test("at-least-one-number", "Contain at least one number", (value) => {
+      if (!value) return false;
+      const regex = /^(?=.*\d)/;
+      return regex.test(value);
+    }),
   confirmPassword: Yup.string()
     .required()
     .oneOf([Yup.ref("password"), ""], "Password not match"),
